8de4e1b173d64e62184dc51e98ba7ec7cec9f87a
Evald <e.denhof@chitinsoftware.de>
2025-04-26 05:08:16 +0200

    mobile HederSection adjusted

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './App.css';
-import './index.css';
-import HeaderSection from './sections/HeaderSection';
-
-
-
+import "./App.css";
+import "./index.css";
+import HeaderSection from "./sections/HeaderSection";
 
 function App() {
   return (
-   <div class="z-10">
-    <p class='z-10   text-white mx-12 my-4 text-xl'>Evald Denhof</p>
-    <div class="absolute top-96 left-[-7vh] z-0 rounded-full bg-white h-[70vh] w-[70vh] bg-gradient-to-br from-orange-300 from-40% to-red-400 to-80%"></div>
- <HeaderSection/>
+    <div class="z-10">
+     
+      <p class="z-10 text-white mx-4 md:mx-16 my-4 text-xl flex items-center gap-2">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+       stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round"
+          d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
+  </svg>
+  Evald Denhof
+</p>
 
-   </div>
+      
+
+      <div class=" absolute top-96 left-[-7vh] md:top-96 md:left-[-7vh] z-0 rounded-full bg-white h-[20vh] w-[20vh] md:h-[70vh] md:w-[70vh] bg-gradient-to-br from-orange-300 from-40% to-red-400 to-80%"></div>
+      <HeaderSection />
+    </div>
   );
 }
 
--- src/sections/HeaderSection.js
+++ src/sections/HeaderSection.js
@@ -1,20 +1,25 @@
 import { Button } from "bootstrap";
 import evald from "../assets/evald.jpg";
-import '../index.css';
-import '../App.css';
-
+import "../index.css";
+import "../App.css";
 
 export default function HeaderSection() {
   return (
-    <div class="z-20 flex col-auto bg-gray-600 mx-12 my-5 min-h-[90vh] rounded-2xl bg-gray-0 backdrop-filter backdrop-blur-md bg-opacity-0 border border-gray-100 border-opacity-50">
-      <img src={evald} class="my-auto mx-10 rounded-2xl max-h-[80vh]"></img>
-      <div class="mx-32 my-auto text-2xl">
+    <div class="z-20 grid grid-flow-row md:flex md:col-auto bg-gray-600 mx-4 md:mx-16 my-5 min-h-[85vh]  rounded-2xl bg-gray-0 backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100 border-opacity-50">
+      <img
+        src={evald}
+        class="border-orange-300 border my-auto mx-auto md:mx-10 rounded-2xl max-h-[50vh] md:max-h-[80vh]"
+      ></img>
+      <div class="mx-auto md:mx-24 md:my-auto text-2xl md:text-2xl">
         <p class="text-white ">Hi im Evald Denhof</p>
-        <p class="text-white text-6xl font-bold ">Im <span class="text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-red-500">Fullstack</span></p>
-        <p class="text-white text-6xl font-bold ">Junior Developer</p>
-        <div class="col-auto my-10">
-
-        </div>
+        <p class="text-white text-4xl md:text-6xl font-bold ">
+          Im{" "}
+          <span class="text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-red-500">
+            Fullstack
+          </span>
+        </p>
+        <p class="text-white text-4xl md:text-6xl font-bold ">Junior Developer</p>
+        <div class="col-auto my-10"></div>
       </div>
     </div>
   );
